f2cf21c484fab7a7d0b10496e0f74b8966fbd914
Mike Smith <mike@noiseparticles.com>
2020-11-19 10:36:21 -0500

    adding parent search component

---
+++ src/app/components/application/personal-info/parent/parent-search/parent-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'iee-parent-search',
+  templateUrl: './parent-search.component.html',
+  styleUrls: ['./parent-search.component.css']
+})
+export class ParentSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
