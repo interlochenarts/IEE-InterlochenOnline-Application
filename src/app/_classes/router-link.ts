71cf812df325a051d5024cf856562690705265d3
Mike Smith <mike@noiseparticles.com>
2021-02-01 14:32:21 -0500

    add back/next save links

---
+++ src/app/_classes/router-link.ts
@@ -0,0 +1,9 @@
+export class RouterLink {
+  routerLink: string;
+  text: string;
+
+  constructor(routerLink: string, text: string) {
+    this.routerLink = routerLink;
+    this.text = text;
+  }
+}
