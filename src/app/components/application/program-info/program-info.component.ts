fd76a3a4ffdbd5d538fec0a7678e4922cb8a22d8
imbmoconno <brian.oconnor@interlochen.org>
2021-03-18 14:17:42 -0400

    Fix a console error that has been bugging me while testing

--- src/app/components/application/program-info/program-info.component.ts
+++ src/app/components/application/program-info/program-info.component.ts
@@ -95,7 +95,7 @@ export class ProgramInfoComponent implements OnInit {
   updateSelectedDivision(): void {
     const originalDivision: string = this.appData?.programData?.selectedDivision;
     const grade = this.appData.programData.gradeInSchool;
-    const gradeNumber = grade.match(/\d+/);
+    const gradeNumber = grade?.match(/\d+/);
     this.appData.programData.selectedDivision = this.appData.programData.divisionGradeMap.get(+gradeNumber);
     if (originalDivision && (originalDivision !== this.appData.programData.selectedDivision)) {
       this.clearSelectedPrograms();
