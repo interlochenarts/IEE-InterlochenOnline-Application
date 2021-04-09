318a7cf0ea5b3afed41107b2f91ca9331c475b3c
Mike Smith <mike@noiseparticles.com>
2021-04-09 09:36:56 -0400

    new node scripts

---
+++ gocd_scripts/nvm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ..;
+
+# check if nvm is installed. If not, install it.
+if [ ! -f "$HOME/.nvm/nvm.sh" ]; then
+    # May need to be updated with the latest nvm release
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+fi
+
+# lets us use the nvm commands
+source "$HOME/.nvm/nvm.sh"
+nvm install 15
