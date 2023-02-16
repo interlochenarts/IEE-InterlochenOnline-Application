84324b9cefce20bcd71f902920e95cce7bc8069d
Michael Smith <smithmd@interlochen.org>
2023-02-16 12:21:38 -0500

    add nvm to deploy

--- gocd_scripts/deploy.sh
+++ gocd_scripts/deploy.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-npm install -s
+NODE_VERSION=14
+NG_CLI_VERSION=13.3.2
+
+cd ..;
+
+# check if nvm is installed. If not, install it.
+if [ ! -f "$HOME/.nvm/nvm.sh" ]; then
+    # May need to be updated with the latest nvm release
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+fi
+
+# lets us use the nvm commands
+source "$HOME/.nvm/nvm.sh"
+nvm install $NODE_VERSION
+
+# get our dependencies
+echo -e "===> npm install <===\n"
+npm install -s;
 
 if [[ -z "${SFDC_CONSUMER_KEY}" ]]; then
   echo -e "Missing SFDC_CONSUMER_KEY environment variable"
@@ -22,6 +39,9 @@ if [[ -z "${KEY_FILE}" ]]; then
   echo -e "Missing KEY_FILE environment variable"
 fi
 
+
+
+echo -e "===> SFDX Deploy <===\n"
 sfdx -v
 
 echo -e "sfdx force:auth:jwt:grant -i${SFDC_CONSUMER_KEY} -f/home/wwadmin/certificates/${KEY_FILE} -u${sfdcUser} -a${DX_ENV} -rhttps://${LOGIN_SERVER}.salesforce.com"
