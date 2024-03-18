53116daa2d9ea7b1060565dc376f8afe7f3fff02
Mike Smith <smithmd@interlochen.org>
2024-03-18 16:50:59 -0400

    force course sequence and make sure the correct error is displayed

--- src/app/components/application/program-tabs/certificate-info/bundle-modal/bundle-modal.component.ts
+++ src/app/components/application/program-tabs/certificate-info/bundle-modal/bundle-modal.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CertificateGroup} from "../../../../../_classes/certificate-group";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {Program} from '../../../../../_classes/program';
 
 @Component({
   selector: 'iee-bundle-modal',
@@ -12,9 +13,23 @@ export class BundleModalComponent implements OnInit {
   @Input() group: CertificateGroup;
   @Input() selectedDivision: string;
   isValid: boolean;
+  allSelected: boolean;
+  allInSequence: boolean;
 
-  get bundleChoiceString(): string  {
+  get bundleChoiceString(): string {
     return this.group.bundleChoices.join(';');
+  }
+
+  get selectedIOCourses() {
+    const selectedPrograms: Program[] = [];
+    selectedPrograms.length = this.group.bundleSize;
+    this.group.bundleChoices.forEach((id: string, index: number) => {
+      const program = this.group.courses[index].programsByDivision.get(this.selectedDivision).find(p => p.id === id);
+      if (program) {
+        selectedPrograms[index] = program;
+      }
+    });
+    return selectedPrograms;
   }
 
   ngOnInit() {
@@ -28,18 +43,39 @@ export class BundleModalComponent implements OnInit {
   ok() {
     // console.dir(this.group.bundleChoices);
     let allSelected = true;
+    let allOrderedInSuccession = true;
+    let previousSessionStart = -1;
     for (let i = 0; i < this.group.bundleSize; i++) {
+      // check to make sure there are no program slots empty
       if (!this.group.bundleChoices[i]) {
         allSelected = false;
-        break;
+      }
+
+      // check to make sure an ordered course is not selected before its previous course
+      const program = this.selectedIOCourses[i];
+      if (program && program.sessionStartDate && this.group.courses[i].displayOrder) {
+        // only care if the session has a start date and there is an order
+        if (program.sessionStartDate <= previousSessionStart) {
+          // if this program has a startDate before the previous one, fail
+          allOrderedInSuccession = false;
+        }
+        previousSessionStart = program.sessionStartDate;
       }
     }
-    this.isValid = this.group.bundleChoices.length === this.group.bundleSize && allSelected;
+
+    this.allSelected = this.group.bundleChoices.length === this.group.bundleSize && allSelected;
+    this.allInSequence = allOrderedInSuccession;
+
+    this.isValid = this.allSelected && this.allInSequence;
 
     if (this.isValid) {
       this.modal.close(this.bundleChoiceString);
     } else {
-      console.error('NOT ENOUGH OPTIONS CHOSEN')
+      if (!(this.group.bundleChoices.length === this.group.bundleSize && allSelected)) {
+        console.error('NOT ENOUGH OPTIONS CHOSEN');
+      } else if (!allOrderedInSuccession) {
+        console.error('OPTIONS CHOSEN OUT OF SEQUENCE');
+      }
     }
   }
 }
