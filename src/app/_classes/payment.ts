8007e59bc954bbeb931721b154ebd90f1a4a51bf
imbmoconno <brian.oconnor@interlochen.org>
2024-03-19 13:08:28 -0400

    * IO Bundle Payments

--- src/app/_classes/payment.ts
+++ src/app/_classes/payment.ts
@@ -1,13 +1,17 @@
+import {PaymentBundle} from './payment-bundle';
+import {PaymentAppChoice} from './payment-appchoice';
+
 export class Payment {
   tuitionPaid: boolean;
   paidOnLoad: boolean;
   amountOwed: number;
   amountPaid: number;
+  pendingCredit: number;
   cancelAmount: number;
   payments: Array<string>;
-  creditPayments: Array<string>;
   pendingDiscounts: Array<string>;
-  appliedDiscounts: Array<string>;
+  pendingBundles: Array<PaymentBundle>;
+  pendingAppChoices: Array<PaymentAppChoice>;
   credits: number;
   ccFee: number;
   ccPercent: number;
@@ -20,9 +24,18 @@ export class Payment {
   appliedWaivers: number;
   hasWaiverTransactions: boolean;
 
+  constructor() {
+    this.pendingBundles = new Array<PaymentBundle>();
+    this.pendingAppChoices = new Array<PaymentAppChoice>();
+  }
+
   public static createFromNestedJson(json: any): Payment {
     const payment = new Payment();
     Object.assign(payment, json);
+
+    payment.pendingBundles = json.pendingBundles?.map((b) => PaymentBundle.createFromNestedJson(b));
+    payment.pendingAppChoices = json.pendingAppChoices?.map((ac) => PaymentAppChoice.createFromNestedJson(ac));
+
     // Don't let them use more credit than they owe
     payment.spendableCredit = payment.credits > 0 && payment.credits > payment.amountOwed ? payment.amountOwed : payment.credits;
     // Always default to false, even if they have already applied credit
