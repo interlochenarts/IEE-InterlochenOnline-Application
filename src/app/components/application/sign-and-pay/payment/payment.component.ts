9b3ba8ca9b9377faa540c34fa0399f2ccefd02b3
imbmoconno <brian.oconnor@interlochen.org>
2024-04-09 13:32:37 -0400

    * Force one additional run of the timer to ensure future work has finished

--- src/app/components/application/sign-and-pay/payment/payment.component.ts
+++ src/app/components/application/sign-and-pay/payment/payment.component.ts
@@ -29,6 +29,7 @@ export class PaymentComponent implements OnInit, OnDestroy {
   enteredCode: string;
   totalTuition: number;
   timer: number;
+  firstLoad: boolean = true;
 
   userType = 'student';
   credentialStatus: string;
@@ -228,7 +229,7 @@ export class PaymentComponent implements OnInit, OnDestroy {
           this.hasCode = this.appData.payment.waiverCode != null;
           this.totalTuition = this.appData.payment.appliedCredits + this.appData.payment.appliedWaivers;
           this.totalTuition += this.appData.payment.amountOwed + this.appData.payment.amountPaid;
-          if (this.appData.payment.amountOwed <= 0) {
+          if (this.appData.payment.amountOwed <= 0 && !this.firstLoad) {
             clearInterval(this.timer);
 
             this.selectedPrograms?.forEach(program => { program.registeredDate = new Date().toLocaleDateString()});
@@ -251,6 +252,9 @@ export class PaymentComponent implements OnInit, OnDestroy {
             this.appDataService.transactionId.next(null);
 
             this.isLoading = false;
+            // This firstLoad check lets it run the timer one last time, giving future methods enough time to finish
+          } else if (this.appData.payment.amountOwed <= 0 && this.firstLoad) {
+            this.firstLoad = false;
           }
         }
       },
